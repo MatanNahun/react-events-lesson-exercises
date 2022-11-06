acc19291f20c3a8fe68d5ba1aded9cbed6de5790
MatanNahum <matan.nahum95@gmail.com>
2022-11-06 14:36:19 +0200

    spot check 1 done

--- src/components/SpotCheck1/SpotCheck1.js
+++ src/components/SpotCheck1/SpotCheck1.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SpotCheck1 extends Component {
-    //add another method called logHover, 
-    //which prints "I was hovered!" when the *mouse enters* the button
-    logClick() {
-        console.log("I was clicked!")
-    }
-    render() {
-        return (
-            <button id="logger" onClick={this.logClick}>Click me</button>
-        );
-    }
+  //add another method called logHover,
+  //which prints "I was hovered!" when the *mouse enters* the button
+  logHover() {
+    console.log("I was hovered!");
+  }
+
+  logClick() {
+    console.log("I was clicked!");
+  }
+  render() {
+    return (
+      <button id="logger" onMouseEnter={this.logHover} onClick={this.logClick}>
+        Click me
+      </button>
+    );
+  }
 }
 
 export default SpotCheck1;
