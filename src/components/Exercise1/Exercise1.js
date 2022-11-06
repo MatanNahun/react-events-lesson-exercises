25812bfcf5f9c0e635fb0938f17ba69f220abefd
MatanNahum <matan.nahum95@gmail.com>
2022-11-06 18:47:47 +0200

    ex 1 done

--- src/components/Exercise1/Exercise1.js
+++ src/components/Exercise1/Exercise1.js
@@ -1,31 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise1 extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       images: [
         "http://www.agr.gc.ca/resources/prod/img/discover-decouvrez/potatoes300x210.jpg",
-        "https://img.purch.com/rc/300x200/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA3Ni85MTYvb3JpZ2luYWwvc3BpbmFjaC5qcGc=",
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/PerfectStrawberry.jpg/220px-PerfectStrawberry.jpg"],
-      currentImg: 0
-    }
+        "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/PerfectStrawberry.jpg/220px-PerfectStrawberry.jpg",
+      ],
+      currentImg: 0,
+    };
   }
   shiftImageBack = () => {
     // this should reduce currentImg by 1
-  }
+    let newCurrentImg = this.state.currentImg - 1;
+    if (newCurrentImg < 0) {
+      return;
+    } else {
+      this.setState({ currentImg: newCurrentImg });
+    }
+  };
   shiftImageForward = () => {
     // shiftImageForward - this should increase currentImg by 1
-  }
+    let newCurrentImg = this.state.currentImg + 1;
+    if (newCurrentImg === this.state.images.length) {
+      this.setState({ currentImg: 0 });
+    } else {
+      this.setState({ currentImg: newCurrentImg });
+    }
+  };
 
   render() {
     return (
-      <div >
-        {/* render two buttons with the classes "back" and "forward",
-           and the image at index currentImg, in an img tag */}
+      <div>
+        <button onClick={this.shiftImageBack}>back</button>
+        <img
+          alt="error with image index"
+          width={200}
+          height={200}
+          src={this.state.images[this.state["currentImg"]]}
+        ></img>
+        <button onClick={this.shiftImageForward}>forward</button>
       </div>
     );
   }
 }
+
+//   render() {
+//     return this.state.images.map((imageLink) => (
+//       <div>
+//         <button onClick={this.shiftImageBack}>back</button>
+//         <img width={200} height={200} src={imageLink} alt="nada"></img>
+//         <button onClick={this.shiftImageForward}>forward</button>
+//       </div>
+//     ));
+//   }
+// }
 
 export default Exercise1;
