dbc80a55b754ac06979ce7950e22681014cbd058
MatanNahum <matan.nahum95@gmail.com>
2022-11-06 18:00:35 +0200

    spot check 5 done

--- src/components/SpotCheck5/SpotCheck5.js
+++ src/components/SpotCheck5/SpotCheck5.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Task from "./Task";
 
 class SpotCheck5 extends Component {
-
   // Each task should be desplayed with a Task component.
   // Each Task should display the task's text, and a Complete button with the class "complete"
   // However, SpotCheck5 should filter out the tasks whose complete is true
   // Clicking the Complete button should change that task's complete to true in App's state
 
   constructor() {
-    super()
+    super();
     this.state = {
       tasks: [
         { text: "Take out trash", complete: false },
         { text: "Trash talk Carrie", complete: true },
-        { text: "Carry boxes upstairs", complete: false }
-      ]
-    }
+        { text: "Carry boxes upstairs", complete: false },
+      ],
+    };
   }
 
-  markComplete = text => {
-
-  }
+  markComplete = (text) => {
+    let currentTasks = [...this.state.tasks];
+    currentTasks.find((task) => task["text"] === text).complete = true;
+    this.setState({ tasks: currentTasks });
+  };
 
   render() {
-    return (
-      <div></div>
-    )
+    return this.state.tasks
+      .filter((task) => !task.complete)
+      .map((t) => <Task task={t} markComplete={this.markComplete} />);
   }
 }
 
--- src/components/SpotCheck5/Task.js
+++ src/components/SpotCheck5/Task.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Task extends Component {
   complete = () => {
-
-  }
+    this.props.markComplete(this.props.task.text);
+  };
 
   render() {
     return (
       <div>
-
+        {this.props.task.text} -{" "}
+        <button onClick={this.complete}>Complete</button>
       </div>
-    )
+    );
   }
 }
 
